52c370857a8fa0717466702dff8edccadd982997
sahu-virendra-1908 <virendrashivsahu@gmail.com>
2026-03-11 12:59:39 +0530

    v

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://secure-z6ql.onrender.com",
+    origin: "https://secure-1-zokw.onrender.com",
   },
 });
 
